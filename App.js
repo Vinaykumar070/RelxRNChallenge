5c7da513c7ad6dbb903bc7227391c3854a597cb4
vinaykumar <vkoudi@alamanceinc.com>
2023-06-18 18:42:13 +0530

    search post offices

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
-import {FlatList, View, StyleSheet, StatusBar, SafeAreaView, Text} from 'react-native'
-import axios from "axios";
-
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {
+  FlatList,
+  View,
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import axios from 'axios';
 
 const Item = ({title}) => (
   <View style={styles.item}>
-    <Text style={styles.title}>Name: {title.Name}</Text>
-    <Text style={styles.title}>Description: {title.Circle}</Text>
+    <Text style={styles.title}>{title.Name}</Text>
+    <Text style={styles.title}>{title.BranchType}</Text>
+    <Text style={styles.title}>{title.State}</Text>
+    <Text style={styles.title}>{title.Pincode}</Text>
   </View>
 );
 
@@ -15,26 +25,50 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "React/Parcel Starter",
-      persons: []
+      message: 'React/Parcel Starter',
+      persons: [],
+      value: '',
     };
   }
-  componentDidMount() {
-    axios.get(`https://api.postalpincode.in/postoffice/tadas`).then(res => {
+  searchPostOffices = () => {
+    axios
+      .get(`https://api.postalpincode.in/postoffice/${this.state.value}`)
+      .then(res => {
+        if (res.status === 200) {
+          if (res.data[0].Status === 'Success') {
+            this.setState({persons: res.data[0].PostOffice});
+          } else if (res.data[0].Status === 'Error') {
+            alert(res.data[0].Message);
+          }
+        } else if (res.status >= 500 || res.status === 400) {
+          alert('Something went wrong, please try agin later.');
+        }
+      })
+      .catch(err => {
+        alert(err);
+      });
+  };
 
-      this.setState({ persons : res.data[0].PostOffice });
-    });
-  }
-  
   render() {
     return (
       <SafeAreaView style={styles.container}>
-      <FlatList
-        data={this.state.persons}
-        renderItem={({item}) => <Item title={item} />}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
+        <View style={{flexDirection: 'row'}}>
+          <TextInput
+            placeholder="Search Post office"
+            style={styles.inputForm}
+            onChangeText={value => this.setState({value})}></TextInput>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.searchPostOffices}>
+            <Text style={styles.text}>Search</Text>
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={this.state.persons}
+          renderItem={({item}) => <Item title={item} />}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
     );
   }
 }
@@ -45,14 +79,43 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: '#FCFCFC',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-    flexDirection: 'column'
+    flexDirection: 'column',
+    borderRadius: 16,
+    borderColor: '#FF8333',
+    borderWidth: 1,
   },
   title: {
-    fontSize: 15,
+    fontSize: 18,
+  },
+  inputForm: {
+    borderWidth: 1,
+    borderRadius: 100,
+    marginVertical: 16,
+    width: '60%',
+    marginLeft: '5%',
+    borderColor: '#FF8333',
+    marginLeft: ' 5%',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#FF8333',
+    padding: 10,
+    width: '25%',
+    height: '60%',
+    marginTop: '4%',
+    marginLeft: '2%',
+    borderRadius: 25,
+  },
+  text: {
+    textAlig: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontSize: 20,
   },
 });
 
